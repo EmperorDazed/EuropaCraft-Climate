b28e0135902be6826cc22593e819a7056593243e
EmperorDazed <filipzybajlo@outlook.com>
2026-09-01 16:26:55 +0100

    Delete null1.js

--- null1.js
+++
@@ -1,9 +0,0 @@
-
-/*
- * EuropaCraft Weather Rework 8
- *
- * Intentionally unused legacy placeholder.
- *
- * This file is NOT loaded by index.html.
- * No Rework 8 functionality depends on it.
- */
